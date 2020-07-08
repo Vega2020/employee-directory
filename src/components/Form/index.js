754ff5159156c7cadba8a480e76a79f74dc29545
Jeremy Flowers <flowersje08@gmail.com>
2020-07-07 17:47:32 -0700

    updated form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,7 +5,6 @@ class Form extends Component {
   state = {
     firstName: "",
     lastName: "",
-    password: ""
   };
 
   handleInputChange = event => {
@@ -13,9 +12,6 @@ class Form extends Component {
     let value = event.target.value;
     const name = event.target.name;
 
-    if (name === "password") {
-      value = value.substring(0, 15);
-    }
     // Updating the input's state
     this.setState({
       [name]: value
@@ -26,21 +22,17 @@ class Form extends Component {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
     
-    if (!this.state.firstName || !this.state.lastName) {
-      alert("Fill out your first and last name please!");
-    } else if (this.state.password.length < 6) {
-      alert(
-        `Choose a more secure password ${this.state.firstName} ${this.state
-          .lastName}`
-      );
+    // If both fields are blank, the user will be asked to fill at least one of them.
+    if (!this.state.firstName && !this.state.lastName) {
+      alert("Fill out a first or last name please!");
     } else {
+      // Code for processing the search input will go here.
       alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
     }
 
     this.setState({
       firstName: "",
       lastName: "",
-      password: ""
     });
   };
 
@@ -65,13 +57,6 @@ class Form extends Component {
             onChange={this.handleInputChange}
             type="text"
             placeholder="Last Name"
-          />
-          <input
-            value={this.state.password}
-            name="password"
-            onChange={this.handleInputChange}
-            type="password"
-            placeholder="Password"
           />
           <button onClick={this.handleFormSubmit}>Submit</button>
         </form>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // This is what's actually being rendered to the screen when we load the app. 
 ReactDOM.render(
